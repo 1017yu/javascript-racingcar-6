f991e500ab448fa66cdf751ce0b576157a67ae1a
Kled <1017yu@daum.net>
2023-10-27 19:39:43 +0900

    feat: 시도할 횟수 입력 값 `laps` 유효성 검사 기능 추가

--- src/models/InputValidator.js
+++ src/models/InputValidator.js
@@ -1,17 +1,31 @@
-import { ERROR_MESSAGES, rSpecialChar } from '../constants/index.js';
+import { ERROR_MESSAGES, rSpecialChar } from '../constants/messages.js';
+import OPTIONS from '../constants/options.js';
 
 class InputValidator {
-  static validateCarNames(carNames) {
+  /**
+   * 자동차 이름 입력에 대한 유효성 체크
+   * @param {string[]} carNames
+   */
+  static carNames(carNames) {
     const nameSet = new Set();
+    const { exceedName, emptyName, specialChar, duplicatedName } =
+      ERROR_MESSAGES;
 
+    if (carNames.length < 2) throw new Error(exceedName);
     carNames.forEach(el => {
-      if (el.length > 5) throw new Error(ERROR_MESSAGES.tooLongName);
-      if (!el.length) throw new Error(ERROR_MESSAGES.emptyName);
-      if (rSpecialChar.test(el)) throw new Error(ERROR_MESSAGES.noSpecialChar);
-      if (nameSet.has(el)) throw new Error(ERROR_MESSAGES.noDuplicateName);
-
+      if (el.length > OPTIONS.maxNaming) throw new Error(exceedName);
+      if (!el.length) throw new Error(emptyName);
+      if (rSpecialChar.test(el)) throw new Error(specialChar);
+      if (nameSet.has(el)) throw new Error(duplicatedName);
       nameSet.add(el);
     });
+  }
+
+  static laps(number) {
+    const { positive, integer } = ERROR_MESSAGES;
+
+    if (number < OPTIONS.minLaps) throw new Error(positive);
+    if (Number.isNaN(number)) throw new Error(integer);
   }
 }
 
