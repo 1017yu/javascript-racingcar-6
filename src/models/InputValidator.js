ba006fdc24c49a6f97595b202778d4d0e2ef2801
Kled <1017yu@daum.net>
2023-10-27 21:21:31 +0900

    feat: 시도할 횟수 입력 값 유효성 판별 메소드 추가

--- src/models/InputValidator.js
+++ src/models/InputValidator.js
@@ -1,4 +1,5 @@
-import { ERROR_MESSAGES, rSpecialChar } from '../constants/messages.js';
+import { ERROR_MESSAGES } from '../constants/messages.js';
+import REGEXS from '../constants/regexs.js';
 import OPTIONS from '../constants/options.js';
 
 class InputValidator {
@@ -7,25 +8,31 @@ class InputValidator {
    * @param {string[]} carNames
    */
   static carNames(carNames) {
-    const nameSet = new Set();
     const { exceedName, emptyName, specialChar, duplicatedName } =
       ERROR_MESSAGES;
+    const { rSpecialChar } = REGEXS;
+    const { maxNaming } = OPTIONS;
 
-    if (carNames.length < 2) throw new Error(exceedName);
-    carNames.forEach(el => {
-      if (el.length > OPTIONS.maxNaming) throw new Error(exceedName);
-      if (!el.length) throw new Error(emptyName);
-      if (rSpecialChar.test(el)) throw new Error(specialChar);
-      if (nameSet.has(el)) throw new Error(duplicatedName);
-      nameSet.add(el);
+    carNames.forEach((name, index) => {
+      if (index > 2) throw new Error(exceedName);
+      if (name.length > maxNaming) throw new Error(exceedName);
+      if (!name.length) throw new Error(emptyName);
+      if (rSpecialChar.test(name)) throw new Error(specialChar);
+      if (index !== carNames.indexOf(name)) throw new Error(duplicatedName);
     });
   }
 
-  static laps(number) {
+  /**
+   * 자동차 이름 입력에 대한 유효성 체크
+   * @param {number} laps
+   */
+  static laps(laps) {
     const { positive, integer } = ERROR_MESSAGES;
+    const { rWhiteSpace } = REGEXS;
 
-    if (number < OPTIONS.minLaps) throw new Error(positive);
-    if (Number.isNaN(number)) throw new Error(integer);
+    if (rWhiteSpace.test(laps)) throw new Error('에러');
+    if (parseInt(laps, 10) < OPTIONS.minLaps) throw new Error(positive);
+    if (Number.isNaN(parseInt(laps, 10))) throw new Error(integer);
   }
 }
 
