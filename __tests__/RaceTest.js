c56c8b9a01561c56c75c4cfd3b9468f43c1e4801
Kled <1017yu@daum.net>
2023-10-30 01:01:05 +0900

    fix: scoreBoard 테스트 케이스 및 메소드 변경

--- __tests__/RaceTest.js
+++ __tests__/RaceTest.js
@@ -1,5 +1,4 @@
 import Race from '../src/models/Race';
-import { Console } from '@woowacourse/mission-utils';
 
 describe('Race 실행 결과', () => {
   let race;
@@ -8,16 +7,21 @@ describe('Race 실행 결과', () => {
 
   beforeEach(() => {
     race = new Race();
-    scoreBoard = { tesla: 0, benz: 0 };
+    scoreBoard = [
+      ['tesla', 0],
+      ['benz', 0],
+    ];
     laps = 3;
   });
 
   test('`racing` 메소드 테스트', () => {
     const result = race.racing(scoreBoard, laps);
 
-    expect(result).toMatchObject([
-      ['tesla', expect.any(Number)],
-      ['benz', expect.any(Number)],
-    ]);
+    expect(result).toEqual(
+      expect.arrayContaining([
+        ['tesla', expect.any(Number)],
+        ['benz', expect.any(Number)],
+      ]),
+    );
   });
 });
